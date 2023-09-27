4b2f814c856e82d226babbb3c5e118e10a7b29ba
Alessandro Cipriani <social@magiaslab.com>
2023-09-27 19:09:30 +0200

    v

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -9,7 +9,7 @@ export default function Greeting({messages}) {
   return (
     <div>
       <h3>{greeting}! Thank you for visiting!</h3>
-      <button onClick={() => setGreeting(randomMessage())}>
+      <button class="btn btn-blue" onClick={() => setGreeting(randomMessage())}>
         New Greeting
       </button>
     </div>
